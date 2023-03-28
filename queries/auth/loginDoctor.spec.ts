8ea8d714117653e037e32f84dd4165e364cfa2ac
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-03-27 22:58:25 -0400

    added coverage, e2e tests and unit tests

---
+++ queries/auth/loginDoctor.spec.ts
@@ -0,0 +1,47 @@
+import { loginDoctor } from './loginDoctor';
+import { StatusCodes } from 'http-status-codes';
+import { registerDoctor } from './registerDoctor';
+describe("TEST LOGIN DOCTOR QUERY", ()=>{
+    it("SHOULD FAIL if user is not found", async()=>{
+        // create user
+        const testUser = {
+            username: "Invalid",
+            password: "Invalid"
+        }
+
+        // login with credential
+        const response = await loginDoctor({username: testUser.username, password: testUser.password})
+
+        // checks
+        expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED)
+        expect(response.result).toBeFalsy()
+        expect(response.success).toBe(false)
+        expect(response.message).toBeTruthy()
+    })
+
+    it("SHOULD PASS when user logs in with valid credentials", async()=>{
+        // create user
+        const testUser = {
+            username: "test",
+            firstname: "firstname",
+            lastname:"lastname",
+            email: "test@gmail.com",
+            password: "test"
+        }
+
+        // registers doctor
+        await registerDoctor(testUser)
+
+        // login with credential
+        const response = await loginDoctor({username: testUser.username, password: testUser.password})
+
+        // checks
+        expect(response.statusCode).toBe(StatusCodes.OK)
+        expect(response.result).toHaveProperty("username")
+        expect(response.result).toHaveProperty("role")
+        expect(response.result!.username).toBe(testUser.username)
+        expect(response.result!.role).toBe("DOCTOR")
+        expect(response.success).toBe(true)
+        expect(response.message).toBeTruthy()
+    })
+})
--- server.ts
+++ server.ts
@@ -1,43 +1,21 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { app } from "./app";
 import configureMongoose from "./mongo/mongoConfig";
-import cors from "cors";
-import helloRouter from "./routes/hello.router";
-import doctorRouter from "./routes/doctor.route";
-import patientRouter from "./routes/patient.route";
-import authRouter from "./routes/auth.route";
-import cookieParser from "cookie-parser";
+
 
 dotenv.config();
 
 async function startServer() {
-    // initialization
-    const app: Express = express();
-    const port = process.env.PORT;
+    // environment variables
+    const PORT = process.env.PORT;
+    const MONGO_URI = process.env.MONGO_URI;
 
     // mongoose configuration
-    await configureMongoose();
+    await configureMongoose(MONGO_URI || "");
 
-    // middleware
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(
-        cors({
-            origin: "http://localhost:3000",
-            methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-            credentials: true,
-        })
-    );
-    app.use(cookieParser());
-
-    // routers
-    app.use(helloRouter); // THIS WAS JUST FOR TESTING
-    app.use("/doctor/", doctorRouter);
-    app.use("/patient/", patientRouter);
-    app.use("/auth", authRouter);
-    app.listen(port, () => {
+    app.listen(PORT, () => {
         console.log(
-            `⚡️[server]: Server is running at http://localhost:${port}`
+            `⚡️[server]: Server is running at http://localhost:${PORT}`
         );
     });
 }
